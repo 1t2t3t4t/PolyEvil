d710c202ddba6d1338cde83ce6ebdfa1aa525d17
Boss <bossnathakorn@gmail.com>
2022-07-10 13:33:14 +0700

    basic mainmenu ui

---
+++ Assets/UI/CommonColor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace UI
+{
+    public static class CommonColor
+    {
+        public static Color BloodRed()
+        {
+            ColorUtility.TryParseHtmlString("#BE3333", out var outColor);
+            return outColor;
+        }
+    }
+}
