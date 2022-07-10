ff986af787ef3c98bbd552ef79058fd86597a156
Boss <bossnathakorn@gmail.com>
2022-07-10 17:29:23 +0700

    Add basic movement

---
+++ Assets/Scripts/FixedCameraController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Cinemachine;
+using UnityEngine;
+
+public class FixedCameraController : MonoBehaviour
+{
+    [SerializeField] private bool ShouldLookAt = false;
+    
+    private CinemachineBrain _cinemachineBrain;
+    private CinemachineVirtualCamera _virtualCamera;
+
+    private void Start()
+    {
+        _cinemachineBrain = FindObjectOfType<CinemachineBrain>();
+        _virtualCamera = GetComponentInChildren<CinemachineVirtualCamera>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _virtualCamera.LookAt = other.transform;
+            _virtualCamera.enabled = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _virtualCamera.enabled = false;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
+{
+    public float WalkSpeed = 3f;
+    public float RotateSpeed = 3f;
+    
+    private Animator _animator;
+    private CharacterController _characterController;
+    private PlayerAim _playerAim;
+    private bool _isMoving = false;
+
+    private static readonly int Movement = Animator.StringToHash("Movement");
+    private static readonly int IsMoving = Animator.StringToHash("IsMoving");
+
+    public bool Moving => _isMoving;
+    
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _characterController = GetComponent<CharacterController>();
+        _playerAim = GetComponent<PlayerAim>();
+    }
+
+    private void Update()
+    {
+        var verticalMove = Input.GetAxis("Vertical");
+        var horizontalMove = Input.GetAxis("Horizontal");
+
+        var currentTransform = transform;
+        var movementDir = _playerAim.IsAim ? Vector3.zero : currentTransform.forward * verticalMove;
+        _characterController.Move(movementDir.normalized * (WalkSpeed * Time.deltaTime));
+
+        _isMoving = movementDir.sqrMagnitude != 0f;
+
+        var rotation = currentTransform.rotation;
+        var turnRight = Quaternion.Euler(rotation.eulerAngles + (horizontalMove * Vector3.up));
+        var adjustedRotSpeed = _playerAim.IsAim ? RotateSpeed / 2 : RotateSpeed;
+        rotation = Quaternion.RotateTowards(rotation, turnRight, adjustedRotSpeed * Time.deltaTime);
+        currentTransform.rotation = rotation;
+
+        UpdateAnimState();
+    }
+
+    private void UpdateAnimState()
+    {
+        var velocity = _characterController.velocity;
+        var velocityMagnitude = velocity.magnitude;
+        var movement = velocityMagnitude / WalkSpeed;
+        var isForward = Vector3.Dot(transform.forward, velocity) >= 0;
+        _animator.SetFloat(Movement, movement * (isForward ? 1 : -1));
+        _animator.SetBool(IsMoving, _isMoving);
+    }
+}
